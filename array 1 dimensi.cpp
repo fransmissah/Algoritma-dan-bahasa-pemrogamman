b7e314a847dd38662bc03daccb16f8755756f8b7
fransmissah <dandimissah19@gmail.com>
2024-07-13 14:14:29 +0700

    Add files via upload

---
+++ array 1 dimensi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka[8];
+    int jml_arr = sizeof(angka)/sizeof(*angka);
+
+    for(int a = 0; a < jml_arr; a++){
+        cout <<"masukan nilai index ke-"<<a<<" : ";
+        cin>>angka[a];
+
+    }
+    cout<<"===================================================="<<endl;
+    cout<<"nilai yang tersimpan"<<endl;
+    cout<<"===================================================="<<endl;
+    for(int b = 0; b < jml_arr);
+    
+
+}
